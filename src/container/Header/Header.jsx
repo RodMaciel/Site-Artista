9259dfe4ca1fc32c4836da886e60a7e597627421
Rodrigo Maciel <rodrigodmc9@gmail.com>
2023-06-14 05:50:20 -0300

    troca img

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -21,7 +21,7 @@ const Header = () => (
       </button>
     </div>
     <div className="app__wrapper_img">
-      <img src={images.welcome} alt="header img" />
+      <img src={images.indus} alt="header img" />
     </div>
   </div>
 );
